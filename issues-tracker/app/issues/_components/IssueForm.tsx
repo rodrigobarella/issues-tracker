9e75a609ae6c2cbd9b0ab4a037590fc68e8efb62
RBARELLA <rbarella@suzano.com.br>
2024-10-24 21:02:06 -0300

    add delete feature and fix loading

--- issues-tracker/app/issues/_components/IssueForm.tsx
+++ issues-tracker/app/issues/_components/IssueForm.tsx
@@ -28,10 +28,10 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
     try {
       setSubmitting(true);
       if (issue)
-        axios.patch('/api/issues' + issue.id, data);
+        axios.patch('/api/issues/' + issue.id, data);
       else
         await axios.post('/api/issues', data);
-      router.push('/issues');  
+      router.push('/issues/list');  
       router.refresh();   
     } catch (error) {
       setSubmitting(false);
--- issues-tracker/app/layout.tsx
+++ issues-tracker/app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import NavBar from './NavBar'
-import {Theme, ThemePanel} from '@radix-ui/themes';
+import {Container, Theme, ThemePanel} from '@radix-ui/themes';
 
 
 const inter = Inter({
@@ -29,7 +29,9 @@ export default function RootLayout({
       <Theme accentColor="violet">
           <NavBar/>
           <main className='p-5'>
+            <Container>
             {children}
+            </Container>
           </main>
           </Theme>
       </body>
