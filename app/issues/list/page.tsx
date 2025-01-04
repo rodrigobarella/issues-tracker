2477c5a3674489bb6e31d0b96e9e066d349c8c63
RBARELLA <rbarella@suzano.com.br>
2025-01-04 11:27:19 -0300

    filter issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -4,11 +4,24 @@ import prisma from '@/prisma/client';
 import {IssueStatusBadge, Link } from '@/app/components';
 
 import IssueActions from './IssueActions';
+import { Status } from '@prisma/client';
 
+interface Props {
+  searchParams: {status: Status}
+}
 
-const IssuesPage = async () => {
+const IssuesPage = async ({ searchParams }: Props) => {
 
-  const issues = await prisma.issue.findMany()
+  const statuses = Object.values(Status);
+  const status = statuses.includes(searchParams.status) 
+  ? searchParams.status 
+  : undefined;
+
+  const issues = await prisma.issue.findMany({
+    where: {
+      status: status
+    }
+  })
 
   return (
     <div>
