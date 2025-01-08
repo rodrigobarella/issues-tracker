6ef92aa6ee4a84bbbfe852785ecb841c80259ba1
RBARELLA <rbarella@suzano.com.br>
2025-01-08 20:55:41 -0300

    build latest issues component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-import Image from 'next/image'
-import Pagination from './components/Pagination'
+import LatestIssues from './LatestIssues'
 
 
 export default function Home({ searchParams } : { searchParams: { page: string }}) {
   return (
-  <Pagination itemCount={100} pageSize={10} currentPage={parseInt(searchParams.page)}/>
+  <LatestIssues/>
   )
 }
