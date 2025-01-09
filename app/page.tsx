2f12f9722c7ab7335a7e4e54072561d49070f780
RBARELLA <rbarella@suzano.com.br>
2025-01-09 20:51:50 -0300

    build issue summary comp

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
+import prisma from '@/prisma/client'
+import IssueSummary from './IssueSummary'
 import LatestIssues from './LatestIssues'
 
 
-export default function Home({ searchParams } : { searchParams: { page: string }}) {
+export default async function Home({ searchParams } : { searchParams: { page: string }}) {
+
+  const open = await prisma.issue.count({where: {status: 'OPEN'}})
+  const inProgress = await prisma.issue.count({where: {status: 'IN_PROGRESS'}})
+  const closed = await prisma.issue.count({where: {status: 'CLOSED'}})
+
   return (
-  <LatestIssues/>
+  //<LatestIssues/>
+  <IssueSummary open={open} inProgress={inProgress} closed={closed} />
   )
 }
